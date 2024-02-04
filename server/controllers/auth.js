21aa3bb5eb5f2b13712d6311f06a3cbfd7d3514d
UpendraSinghBhadouria <upendrasinghbhadouria75@gmail.com>
2024-02-04 23:05:06 +0530

    cookies error fixed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -29,7 +29,7 @@ export const login = async (req, res, next) => {
         const { password, isAdmin, ...otherDetails } = user._doc;
 
         res.cookie("access_token", token, {
-            httpOnly: true
+            secure: true
         }).status(200).json(otherDetails);
 
     } catch (error) {
